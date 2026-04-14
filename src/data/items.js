b0c5a8d4ccf652ad5979487272308672c451b92f
shafayhsn <shafayhsn@gmail.com>
2026-04-14 15:17:03 +0500

    delete

--- src/data/items.js
+++
@@ -1,6 +0,0 @@
-export const items = [
-  { name: "Stitching Thread - 20/2", uom: "Cone" },
-  { name: "Zipper", uom: "Pcs" },
-  { name: "Button", uom: "Pcs" },
-  { name: "Denim 11062 Blue", uom: "Meter" },
-];
--- src/data/suppliers.js
+++
@@ -1,7 +0,0 @@
-export const suppliers = [
-  { name: "Elite Denim", category: "Fabric", tax: true },
-  { name: "Union Denim", category: "Fabric", tax: true },
-  { name: "Pak Denim", category: "Fabric", tax: true },
-  { name: "Rehmani Thread", category: "Thread", tax: false },
-  { name: "HRK Enterprises", category: "Packing", tax: false },
-];

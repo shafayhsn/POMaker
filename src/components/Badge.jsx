b75ce0458c330574cf2779162b771f0cc0591842
shafayhsn <shafayhsn@gmail.com>
2026-04-14 13:42:33 +0500

    delete

--- src/components/Badge.jsx
+++
@@ -1,5 +0,0 @@
-import { classNames } from '../lib/utils'
-
-export default function Badge({ children, tone = 'default' }) {
-  return <span className={classNames('badge', `badge-${tone}`)}>{children}</span>
-}

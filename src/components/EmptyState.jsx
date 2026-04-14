14c220929dd0c24e5e0f5220bdb0ebb2042b0777
shafayhsn <shafayhsn@gmail.com>
2026-04-14 14:55:25 +0500

    delete

--- src/components/EmptyState.jsx
+++
@@ -1,9 +0,0 @@
-export default function EmptyState({ title, description, action }) {
-  return (
-    <div className="card empty-state stack-sm">
-      <h2>{title}</h2>
-      <p>{description}</p>
-      {action}
-    </div>
-  )
-}
